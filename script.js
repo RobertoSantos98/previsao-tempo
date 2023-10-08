47e9e053cc8b7116fd31c6a41bf480752a509757
RobertoSantos98 <146374934+RobertoSantos98@users.noreply.github.com>
2023-10-08 18:09:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let key = "9d312a89f429b508d5fddbea4973f891"
+
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = 'https://openweathermap.org/img/wn/$dados.weather[0].icon.png'
+}
+
+async function buscarCidade(cidade){
+
+    let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=&{cidade}&appid=&{key}&lang=pt_br&units=metric').then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
